3c6b9fc368ebfcbf09b7e659c871d9f6e74fe689
Joel Smith <joelwsmith123@gmail.com>
2019-01-23 14:43:43 -0700

    Add random palette generator

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,23 @@
+window.onload = function () {
+  generatePaletteElement()
+}
+
+
+const selectSavedBtn = document.querySelector('.select-saved-btn')
+selectSavedBtn.addEventListener('click', function (event) {
+  event.preventDefault()
+  toggleHeaderStyle()
+  toggleSavedPaletteContainerStyle()
+  toggleSelectSavedPaletteContainerStyle()
+})
+
+const generateNewPaletteBtn = document.querySelector('.select-saved-btn')
+generateNewPaletteBtn.addEventListener('click', function (event) {
+  event.preventDefault()
+  generatePaletteElement()
+})
+
+
 function toggleHeaderStyle() {
   const header = document.querySelector('.header')
   
@@ -19,10 +39,33 @@ function toggleSelectSavedPaletteContainerStyle() {
   selectSavedBtnContainer.classList.toggle('select-saved-btn-container-view')
 }
 
-selectSavedBtn.addEventListener('click', function (event) {
-  event.preventDefault()
 
-  toggleHeaderStyle()
-  toggleSavedPaletteContainerStyle()
-  toggleSelectSavedPaletteContainerStyle()
-})
+function generateRandomHexCode() {
+  let randomNumber = () => Math.floor(Math.random() * Math.floor(10))
+  let randomLetter = () => {
+    let letterArr = ['A', 'B', 'C', 'D', 'E', 'F']
+    return letterArr[Math.floor(Math.random() * Math.floor(6))]
+  }
+  let randomDigit = () => { 
+    let selectorNum = Math.floor(Math.random() * Math.floor(2))
+    return selectorNum === 0 ? randomNumber() : randomLetter()
+  }
+
+  let hexCodeArray = []
+  for (let i = 0; i < 6; i++) {
+    hexCodeArray.push(randomDigit())
+  }
+
+  return '#' + hexCodeArray.join('')
+}
+
+function generatePaletteElement() {
+  const paletteContainer = document.querySelector('.palette-container')
+  for (let i = 0; i < 5; i++) {
+    let paletteColorElement = document.createElement('div')
+    paletteColorElement.classList.add('palette-color')
+    paletteColorElement.style.backgroundColor = generateRandomHexCode()
+    paletteContainer.appendChild(paletteColorElement) 
+  }
+}
+
