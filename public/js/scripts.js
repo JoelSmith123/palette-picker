5a9ce157f777b69edc76dd16729dead436469c4f
Joel Smith <joelwsmith123@gmail.com>
2019-01-23 06:45:23 -0700

    Add header transition

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,12 +1,16 @@
+const header = document.querySelector('.header')
 const savedPaletteContainer = document.querySelector('.saved-palette-container')
 const selectSavedBtnContainer = document.querySelector('.select-saved-btn-container')
 const selectSavedBtn = document.querySelector('.select-saved-btn')
 
 selectSavedBtn.addEventListener('click', function (event) {
   event.preventDefault()
-  
-  savedPaletteContainer.classList.toggle('saved-palette-container-view')
+
+  header.classList.toggle('header-hide')
+  header.classList.toggle('header-view')
+
   savedPaletteContainer.classList.toggle('saved-palette-container-hide')
+  savedPaletteContainer.classList.toggle('saved-palette-container-view')
 
   selectSavedBtnContainer.classList.toggle('select-saved-btn-container-hide')
   selectSavedBtnContainer.classList.toggle('select-saved-btn-container-view')
